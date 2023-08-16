1aa0436dd97e48ddec9fe3f8e5da18e72b856916
nabil <nabil_j2bilu56@yahoo.com>
2023-08-16 13:36:16 +0100

    nabil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,14 +21,9 @@
         </style>
     </head>
     <body class="antialiased">
-<<<<<<< HEAD
 
         <h1>New world</h1>
       
-=======
-        <h1>khawla's code</h1>
-
->>>>>>> 3c72ddcd1a40c471e7ccc3a93b2c69bb76fd6324
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
