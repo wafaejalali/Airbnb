7009637a8ec643c282e4843d077a353b39b3638d
nabil <nabil_j2bilu56@yahoo.com>
2023-08-16 13:11:52 +0100

    world

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,8 @@
         </style>
     </head>
     <body class="antialiased">
+
+        <h1>New world</h1>
       
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
